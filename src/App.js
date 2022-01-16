128154f3ea708e207735df1471f1acc8f4ca15f7
Variandr <maxim.savchenko.job@gmail.com>
2022-01-16 20:32:06 +0200

    Added catchError, error for undefined url & Added redirect from default page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import {Route, withRouter} from "react-router-dom";
+import {Redirect, Route, Switch, withRouter} from "react-router-dom";
 import ProfileContainer from "./Content/Profile/profileContainer";
 import AuthContainer from "./Header/headerContainer";
 import Login from "./Content/Login/login";
@@ -14,8 +14,17 @@ const Home = React.lazy(() => import( './Content/Home/home'));
 const UsersContainer = React.lazy(() => import('./Content/Users/usersContainer'));
 
 class App extends React.Component {
+    catchError = (promise) => {
+        console.log("Error message:", promise.reason.message)
+    }
+
     componentDidMount() {
         this.props.initializeApp()
+        window.addEventListener("unhandledrejection", this.catchError)
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener("unhandledrejection", this.catchError)
     }
 
     render() {
@@ -26,10 +35,14 @@ class App extends React.Component {
             <div className='wrapper'>
                 <AuthContainer className='head'/>
                 <div className='content'>
-                    <Route path='/home' render={withSuspense(Home)}/>
-                    <Route path='/profile/:userId?' render={() => <ProfileContainer/>}/>
-                    <Route path='/users' render={withSuspense(UsersContainer)}/>
-                    <Route path='/auth' render={() => <Login/>}/>
+                    <Switch>
+                        <Route exact path="/">{this.props.initialized ? <Redirect to="/profile" /> : <Login/>}</Route>
+                        <Route path='/home' render={withSuspense(Home)}/>
+                        <Route path='/profile/:userId?' render={() => <ProfileContainer/>}/>
+                        <Route path='/users' render={withSuspense(UsersContainer)}/>
+                        <Route path='/auth' render={() => <Login/>}/>
+                        <Route path='*' render={() => <div className='error'><h1>404 Page not found</h1></div>}/>
+                    </Switch>
                 </div>
             </div>
         );
--- src/Content/Users/users.jsx
+++ src/Content/Users/users.jsx
@@ -2,9 +2,10 @@ import React from "react";
 import PageCounter from "../../helpers/pageCounter";
 import User from "./user";
 import FollowingButton from "./followingButton";
+import s from './users.module.css'
 
 const Users = React.memo(({page, onPageChanged, users, follow, unfollow, followingProgress, totalUsers, usersOnPage}) => {
-    return <div>
+    return <div className={s.body}>
         <PageCounter page={page} onPageChanged={onPageChanged} totalUsers={totalUsers} usersOnPage={usersOnPage}/>
         {
             users.map(u => <div key={u.id}>
